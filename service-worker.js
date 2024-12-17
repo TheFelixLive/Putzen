a3285a8369f1aac49c77b73f663858ce6fe54a5e
TheFelixLive <jouloukas@gmail.com>
2024-12-17 15:34:07 +0100

    Testing

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,7 @@
-self.addEventListener('install', event => {
-    console.log('Service Worker installiert.');
-});
-
-self.addEventListener('activate', event => {
-    console.log('Service Worker aktiviert.');
-});
-
-self.addEventListener('notificationclick', event => {
-    console.log('Benachrichtigung geklickt:', event);
-    event.notification.close();
-    // Optional: Handle actions or open a specific URL
-    event.waitUntil(
-        clients.openWindow('https://example.com') // Beispiel-URL
-    );
+self.addEventListener('push', event => {
+    const data = event.data.json();
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: data.icon,
+    });
 });
